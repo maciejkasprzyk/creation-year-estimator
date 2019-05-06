da08fbe27d78ec0f7138d12d3cc7a12874fff45a
vonHousen <42911623+vonHousen@users.noreply.github.com>
2019-05-06 21:05:53 +0200

    Created functions for PL data parsing & extracted date in correct format

--- data_scrapping.py
+++ data_scrapping.py
@@ -1,6 +1,8 @@
 import requests
 import json
-print()
+import re
+from date_parser_pl import *
+
 
 # TODO automate looking for all books at once -> https://wolnelektury.pl/api/books/
 
@@ -18,8 +20,9 @@ parsed_book_info = json.loads(data_book_info)
 
 title = parsed_book_info["title"]
 author = parsed_book_info["authors"][0]["name"]
-author_url = parsed_book_info["authors"][0]["href"]
 epoch = parsed_book_info["epochs"][0]["name"]
+kind = parsed_book_info["kinds"][0]["name"]
+author_url = parsed_book_info["authors"][0]["href"]
 txt_url = parsed_book_info["txt"]
 
 
@@ -31,24 +34,31 @@ parsed_author_info = json.loads(data_author_info)
 
 author_description_ugly = parsed_author_info["description"]
 
-born_date_sign = "Ur."
+birth_date_sign = "Ur."
 date_start_sign = "<dd>"
 date_end_sign = "<br>"
-born_date_area_start_position = author_description_ugly.find(born_date_sign)
+birth_date_area_start_position = author_description_ugly.find(birth_date_sign)
 
-if born_date_area_start_position != -1:
+if birth_date_area_start_position != -1:
 
-    born_date_start_position = \
-        author_description_ugly[born_date_area_start_position:].find(date_start_sign) \
-        + born_date_area_start_position \
+    # find birth date in author's description
+    birth_date_start_position = \
+        author_description_ugly[birth_date_area_start_position:].find(date_start_sign) \
+        + birth_date_area_start_position \
         + len(date_start_sign) \
         + 1
 
-    born_date_length = author_description_ugly[born_date_start_position:].find(date_end_sign)
-    born_date = author_description_ugly[born_date_start_position:born_date_start_position + born_date_length]
+    birth_date_length = author_description_ugly[birth_date_start_position:].find(date_end_sign)
+    birth_date_raw = author_description_ugly[birth_date_start_position:birth_date_start_position + birth_date_length]
+
+    # exclude birth location
+    year_end_position = re.search(r"\b[1-2][0-9]{3}\b", birth_date_raw).end()
+    birth_date_str = birth_date_raw[:year_end_position]
+
+    birth_date = date_parse(birth_date_str)
 
 else:
-    pass                            # TODO find born data on wikipedia
+    pass                            # TODO find birth data on wikipedia
 
 
 death_date_sign = "Zm."
@@ -63,7 +73,13 @@ if death_date_area_start_position != -1:
         + 1
 
     death_date_end_length = author_description_ugly[death_date_start_position:].find(date_end_sign)
-    death_date = author_description_ugly[death_date_start_position:death_date_start_position + death_date_end_length]
+    death_date_raw = author_description_ugly[death_date_start_position:death_date_start_position + death_date_end_length]
+
+    # exclude death location
+    year_end_position = re.search(r"\b[1-2][0-9]{3}\b", death_date_raw).end()
+    death_date_str = death_date_raw[:year_end_position]
+
+    death_date = date_parse(death_date_str)
 
 else:
     pass                            # TODO find death data on wikipedia
@@ -87,18 +103,19 @@ header_last_position = book_without_footer.find(header_sign)
 ISBN_length = 4 + 1 + 13 + 4 + 1
 
 if header_last_position != -1:
-    book_pure_txt = book_without_footer[header_last_position + ISBN_length:]
+    book_pure_txt = book_without_footer[header_last_position + ISBN_length:].strip()
 else:
-    book_pure_txt = book_without_footer
+    book_pure_txt = book_without_footer.strip()
 
 
 # ================== output ===========================
 
 print("Title:\t", title)
-print("Author:\t", author)
+print("Kind:\t", kind)
 print("Epoch:\t", epoch)
-print("Born:\t", born_date)                     # TODO extract pure date only
-print("Died:\t", death_date)                    # TODO extract pure date only
-print("Txt fragment:\t", book_pure_txt[:600])
+print("Author:\t", author)
+print("Born:\t", birth_date)
+print("Died:\t", death_date)
+print("Txt fragment:\n\n", book_pure_txt[:800])
 
 
---
+++ date_parser_pl.py
@@ -0,0 +1,94 @@
+import re
+from datetime import datetime
+
+
+def month_parse(date_raw):
+    """
+    Function parsing month name in PL from wolnelektury.pl into integer.
+    Returns -1 when month doesn't match
+    :param date_raw: date in string format, PL style
+    :return: month number (int)
+    """
+    if date_raw is None:
+        return None
+
+    # exclude year
+    regex_year = re.search(r"\b[1-2][0-9]{3}\b", date_raw)
+    if regex_year is not None:
+        date_without_year = date_raw[:regex_year.start()].strip()
+    else:
+        date_without_year = date_raw
+
+    # exclude day
+    regex_day = re.search(r"\b\d{2}\b", date_raw)
+    if regex_day is not None:
+        month_raw = date_without_year[regex_day.end():]
+    else:
+        month_raw = date_without_year
+
+    # parse polish month names
+    if month_raw.find("sty") != -1:
+        month = 1
+    elif month_raw.find("lut") != -1:
+        month = 2
+    elif month_raw.find("mar") != -1:
+        month = 3
+    elif month_raw.find("kwi") != -1:
+        month = 4
+    elif month_raw.find("maj") != -1:
+        month = 5
+    elif month_raw.find("cze") != -1:
+        month = 6
+    elif month_raw.find("lip") != -1:
+        month = 7
+    elif month_raw.find("sie") != -1:
+        month = 8
+    elif month_raw.find("wrz") != -1:
+        month = 9
+    elif month_raw.find("paź") != -1:
+        month = 10
+    elif month_raw.find("lis") != -1:
+        month = 11
+    elif month_raw.find("gru") != -1:
+        month = 12
+    else:
+        month = -1
+
+    return month
+
+
+def date_parse(date_raw):
+    """
+    Function parsing date from wolnelektury.pl into date format.
+    Returns None if date doesn't match any known style
+    :param date_raw: date in string format, PL style
+    :return: date in correct format
+    """
+
+    if date_raw is None:
+        return None
+
+    # extract year
+    regex_year = re.search(r"\b[1-2][0-9]{3}\b", date_raw)
+    if regex_year is not None:
+        year = regex_year.group()
+    else:
+        return None
+
+    # extract day
+    regex_day = re.search(r"\b\d{1,2}\b", date_raw)
+    if regex_day is not None:
+        day = regex_day.group()
+    else:
+        return None
+
+    # extract month
+    month = month_parse(date_raw)
+    if month is None or month == -1:
+        return None
+
+    date_str = day + "-" + str(month) + "-" + year
+    date = datetime.strptime(date_str, '%d-%m-%Y').date()
+
+    return date
+
