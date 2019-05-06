e0192d23d2d019276185085a7ee9807f5e617049
vonHousen <42911623+vonHousen@users.noreply.github.com>
2019-05-06 10:39:54 +0200

    Extraction of pure txt

--- data-scrapping.py
+++ data-scrapping.py
@@ -18,9 +18,27 @@ txt_url = parsed_book["txt"]
 print(title)
 print(author)
 print(epoch)
-print(txt_url)
+# print(txt_url)
 
 response_txt = requests.get(txt_url)
 data_txt = response_txt.text
 
-print(data_txt)
+footer_sign = "-----"
+footer_position = data_txt.find(footer_sign)
+
+if footer_position != -1:
+    book_without_footer = data_txt[:footer_position]
+else:
+    book_without_footer = data_txt
+
+header_sign = "ISBN"
+header_last_position = book_without_footer.find(header_sign)
+ISBN_length = 4 + 1 + 13 + 4 + 1
+
+if header_last_position != -1:
+    book_pure_txt = book_without_footer[header_last_position + ISBN_length:]
+else:
+    book_pure_txt = book_without_footer
+
+print(book_pure_txt)
+
