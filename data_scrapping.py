2ec28c437307c026bbcc651ae5fb0c59a2361c6d
vonHousen <42911623+vonHousen@users.noreply.github.com>
2019-05-08 00:36:43 +0200

    Automated downloading books, although it is time and disc-space consuming

--- data_scrapping.py
+++ data_scrapping.py
@@ -1,21 +1,19 @@
 import requests
 import json
-import numpy
+import time
 from date_parser_pl import *
 
 
-# TODO automate looking for all books at once -> https://wolnelektury.pl/api/books/
-
-url = "https://wolnelektury.pl/api/books/studnia-i-wahadlo/"
+# url = "https://wolnelektury.pl/api/books/studnia-i-wahadlo/"
 # url = "https://wolnelektury.pl/api/books/pan-tadeusz/"
-# url = "https://wolnelektury.pl/api/books/a-co-wam-spiewac/"     # TODO lyric need to be treated differently to epic
+# url = "https://wolnelektury.pl/api/books/a-co-wam-spiewac/"     # TODO lyric need to be treated differently than epic
 
 
 def get_data_for_book_url(book_url):
     """
     Function get data for given book from wolnelektury.pl
     :param book_url: Link to certain book via API on website: wolnelektury.pl
-    :return: printout
+    :return: scrapped data in dictionary structure
     """
 
     # ================== info scrapping =====================
@@ -35,94 +33,128 @@ def get_data_for_book_url(book_url):
 
     # ================== author info scrapping =============
 
-    response_author_info = requests.get(author_url)
-    data_author_info = response_author_info.text
-    parsed_author_info = json.loads(data_author_info)
+    is_date_found = True
 
-    author_description_ugly = parsed_author_info["description"]
+    if author_url is not None and author_url != '':
 
-    birth_date_sign = "Ur."
-    date_start_sign = "<dd>"
-    date_end_sign = "<br>"
-    birth_date_area_start_position = author_description_ugly.find(birth_date_sign)
+        response_author_info = requests.get(author_url)
+        data_author_info = response_author_info.text
+        parsed_author_info = json.loads(data_author_info)
 
-    if birth_date_area_start_position != -1:
+        author_description_ugly = parsed_author_info["description"]
 
-        # find birth date in author's description
-        birth_date_start_position = \
-            author_description_ugly[birth_date_area_start_position:].find(date_start_sign) \
-            + birth_date_area_start_position \
-            + len(date_start_sign) \
-            + 1
+        birth_date_sign = "Ur."
+        date_start_sign = "<dd>"
+        date_end_sign = "<br>"
+        birth_date_area_start_position = author_description_ugly.find(birth_date_sign)
 
-        birth_date_length = author_description_ugly[birth_date_start_position:].find(date_end_sign)
-        birth_date_raw = author_description_ugly[birth_date_start_position:birth_date_start_position + birth_date_length]
+        if birth_date_area_start_position != -1:
 
-        # exclude birth location
-        year_end_position = re.search(r"\b[1-2]?[0-9]{3}\b", birth_date_raw).end()
-        birth_date_str = birth_date_raw[:year_end_position]
+            # find birth date in author's description
+            birth_date_start_position = \
+                author_description_ugly[birth_date_area_start_position:].find(date_start_sign) \
+                + birth_date_area_start_position \
+                + len(date_start_sign) \
+                + 1
 
-        birth_date = date_parse(birth_date_str)
+            birth_date_length = author_description_ugly[birth_date_start_position:].find(date_end_sign)
+            birth_date_raw = author_description_ugly[birth_date_start_position:birth_date_start_position + birth_date_length]
+
+            # exclude birth location
+            year_end_position = re.search(r"\b[1-2]?[0-9]{3}\b", birth_date_raw).end()
+            birth_date_str = birth_date_raw[:year_end_position]
+
+            birth_date = date_parse(birth_date_str)
+
+        else:
+            is_date_found = False
+
+        death_date_sign = "Zm."
+        death_date_area_start_position = author_description_ugly.find(death_date_sign)
+
+        if death_date_area_start_position != -1:
+
+            death_date_start_position = \
+                author_description_ugly[death_date_area_start_position:].find(date_start_sign) \
+                + death_date_area_start_position \
+                + len(date_start_sign) \
+                + 1
+
+            death_date_end_length = author_description_ugly[death_date_start_position:].find(date_end_sign)
+            death_date_raw = author_description_ugly[
+                                death_date_start_position: death_date_start_position + death_date_end_length
+                             ]
+
+            # exclude death location
+            year_end_position = re.search(r"\b[1-2]?[0-9]{3}\b", death_date_raw).end()
+            death_date_str = death_date_raw[:year_end_position]
+
+            death_date = date_parse(death_date_str)
+
+        else:
+            is_date_found = False
 
     else:
-        pass                            # TODO find birth data on wikipedia
+        is_date_found = False
 
-    death_date_sign = "Zm."
-    death_date_area_start_position = author_description_ugly.find(death_date_sign)
-
-    if death_date_area_start_position != -1:
-
-        death_date_start_position = \
-            author_description_ugly[death_date_area_start_position:].find(date_start_sign) \
-            + death_date_area_start_position \
-            + len(date_start_sign) \
-            + 1
-
-        death_date_end_length = author_description_ugly[death_date_start_position:].find(date_end_sign)
-        death_date_raw = author_description_ugly[
-                            death_date_start_position: death_date_start_position + death_date_end_length
-                         ]
-
-        # exclude death location
-        year_end_position = re.search(r"\b[1-2]?[0-9]{3}\b", death_date_raw).end()
-        death_date_str = death_date_raw[:year_end_position]
-
-        death_date = date_parse(death_date_str)
-
-    else:
-        pass                            # TODO find death data on wikipedia
+    if not is_date_found:
+        birth_date = None  # TODO find birth data on wikipedia
+        death_date = None  # TODO find death data on wikipedia
 
     # ================== txt scrapping =====================
 
-    response_txt = requests.get(txt_url)
-    data_txt = response_txt.text
+    if txt_url is not None and txt_url != '':
 
-    footer_sign = "-----"
-    footer_position = data_txt.find(footer_sign)
+        response_txt = requests.get(txt_url)
+        data_txt = response_txt.text
 
-    if footer_position != -1:
-        book_without_footer = data_txt[:footer_position]
+        footer_sign = "-----"
+        footer_position = data_txt.find(footer_sign)
+
+        if footer_position != -1:
+            book_without_footer = data_txt[:footer_position]
+        else:
+            book_without_footer = data_txt
+
+        header_sign = "ISBN"
+        header_last_position = book_without_footer.find(header_sign)
+        isbn_length = 4 + 1 + 13 + 4 + 1
+
+        if header_last_position != -1:
+            book_pure_txt = book_without_footer[header_last_position + isbn_length:].strip()
+        else:
+            book_pure_txt = book_without_footer.strip()
+
     else:
-        book_without_footer = data_txt
-
-    header_sign = "ISBN"
-    header_last_position = book_without_footer.find(header_sign)
-    isbn_length = 4 + 1 + 13 + 4 + 1
-
-    if header_last_position != -1:
-        book_pure_txt = book_without_footer[header_last_position + isbn_length:].strip()
-    else:
-        book_pure_txt = book_without_footer.strip()
+        book_pure_txt = None
 
     # ================== output ===========================
 
-    print("Title:\t", title)
-    print("Kind:\t", kind)
-    print("Epoch:\t", epoch)
-    print("Author:\t", author)
-    print("Born:\t", birth_date)
-    print("Died:\t", death_date)
-    print("Txt fragment:\n\n", book_pure_txt[:800])
+    # print("Title:\t", title)
+    # print("Kind:\t", kind)
+    # print("Epoch:\t", epoch)
+    # print("Author:\t", author)
+    # print("Born:\t", birth_date)
+    # print("Died:\t", death_date)
+    # print("Txt fragment:\n\n", book_pure_txt[:800])
+
+    if birth_date is not None:
+        birth_date = birth_date.strftime('%d-%m-%Y')
+
+    if death_date is not None:
+        death_date = death_date.strftime('%d-%m-%Y')
+
+    data_dictionary = {
+        "title": title,
+        "kind": kind,
+        "epoch": epoch,
+        "author": author,
+        "birth_date": birth_date,
+        "death_date": death_date,
+        "txt": book_pure_txt
+        }
+
+    return data_dictionary
 
 
 def save_book_list():
@@ -143,11 +175,66 @@ def save_book_list():
             file.write("%s\n" % url)
 
 
-def get_data_for_all_books():
+def save_data_for_books(book_count=-1):
     """
-    Gets data for all books listed in file: url_list.txt
+    Gets data for given number of books listed in file: url_list.txt and saves it in data.json file.
+    When given count < 0 then function loads all books available
+    Function does not load data for books without txt
     :return: -
     """
 
+    with open('url_list.txt', 'r') as file:
+        book_list = file.readlines()
 
-save_book_list()
+    data = []
+    do_for_all = False
+
+    if book_count == 0:
+        return
+    elif book_count < 0:
+        do_for_all = True
+
+    for i, url in enumerate(book_list):
+        if i >= book_count and not do_for_all:
+            break
+        book_data = get_data_for_book_url(url)
+
+        # write only data with text provided
+        if book_data["txt"] is not None:
+            data.append(book_data)
+        else:
+            book_count += 1                                             # save one book more if omitted book without txt
+
+    with open('data.json', 'w') as file:
+        json.dump(data, file)
+
+
+def read_saved_data():
+    """
+    Reads (printout) saved data from data.json file
+    :return: -
+    """
+
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+
+    for book in data:
+        print("\n\n", "-"*30)
+        print("Title:\t", book["title"])
+        print("Kind:\t", book["kind"])
+        print("Epoch:\t", book["epoch"])
+        print("Author:\t", book["author"])
+        print("Born:\t", book["birth_date"])
+        print("Died:\t", book["death_date"])
+        print("Txt fragment:\n\n", book["txt"][:500])
+
+
+# start = time.time()
+#
+# save_data_for_books(20)     # now it loads only 20 books                # TODO do it for all books
+#
+# end = time.time()
+# print("\n", end - start)
+
+
+read_saved_data()
