aebde0c47770c45f9cf13f562d7887f1f4e61af2
Maciej Kasprzyk <kontakt@kasprzykmaciej.pl>
2019-05-27 17:18:27 +0200

    further work on svm

--- paths.py
+++ paths.py
@@ -3,3 +3,13 @@ books_info_path = 'data/books_info.json'
 authors_info_ready_path = 'data/authors_info_ready.json'
 authors_info_raw_path = 'data/authors_info_raw.json'
 books_texts_path = 'data/books_text.json'
+last_grid_search_path = 'models/last_grid_search.pickle'
+best_gird_search_path = 'models/best_grid_search.pickle'
+x_test = 'models/x_test.pickle'
+y_test = 'models/y_test.pickle'
+x_train = 'models/x_training.pickle'
+y_train = 'models/y_training.pickle'
+
+
+
+
--- svm.py
+++ svm.py
@@ -1,20 +1,26 @@
+import pickle
 from pprint import pprint
 from time import time
 
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
-from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 
-import book as bk
-import lemmatization as lm
+import paths
+from book import construct_list_of_books
 
 
 def prepare_class_labels_vector(books):
     labels = []
     for book in books:
         epoch = book.epoch
-        if epoch == "Pozytywizm":
+
+        if epoch == "Starożytność" or epoch == "Średniowiecze":
+            labels.append("Starożytność lub średniowiecze")
+        # elif epoch == "Średniowiecze":
+        #     labels.append("Średniowiecze")
+        elif epoch == "Pozytywizm":
             labels.append("Pozytywizm")
         elif epoch == "Romantyzm":
             labels.append("Romantyzm")
@@ -24,12 +30,8 @@ def prepare_class_labels_vector(books):
             labels.append("Współczesność")
         elif epoch == "Oświecenie":
             labels.append("Oświecenie")
-        elif epoch == "Starożytność" or epoch == "Średniowiecze":
-            labels.append("Starożytność")
         elif epoch == "Dwudziestolecie międzywojenne":
             labels.append("Dwudziestolecie międzywojenne")
-        # elif epoch == "Średniowiecze":
-        #     labels.append("Średniowiecze")
         elif epoch == "Renesans":
             labels.append("Renesans")
         elif epoch == "Barok":
@@ -42,49 +44,42 @@ def prepare_class_labels_vector(books):
 
 def main():
     print("constructing list of books...")
-    books = bk.construct_list_of_books()
+    books = construct_list_of_books()
 
     texts_list = [book.text for book in books]
     labels = prepare_class_labels_vector(books)
 
-    new_texts_list = texts_list
-
-    # new_texts_list = []
-    # i = 0
-    # for txt in texts_list:
-    #     tokenized = lm.tokenize(txt)
-    #     lemmatized = lm.lemmatize(tokenized)
-    #     new_txt = ""
-    #     for word in lemmatized:
-    #         new_txt += word + " "
-    #     new_texts_list.append(new_txt)
-    #     i += 1
-
-    print("vectorizing...")
-    vectorizer = CountVectorizer()
-    # tokenize and build vocab
-
-    pipeline = Pipeline([("vect", CountVectorizer()),
-                         ("tfidf", TfidfTransformer()),
+    pipeline = Pipeline([("tfidf", TfidfVectorizer()),
                          ("svm", SVC(kernel="linear"))])
 
     parameters = {
-        # 'vect__max_df': (0.5, 0.75, 1.0),
-        'vect__max_features': (None, 5000, 10000, 50000),
-        # 'vect__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
+        # 'tfidf__max_df': (0.5, 0.75, 1.0),
+        'tfidf__max_features': (None, 50000),
+        # 'tfidf__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
         # 'tfidf__use_idf': (True, False),
         # 'tfidf__norm': ('l1', 'l2'),
 
     }
 
-    grid_search = GridSearchCV(pipeline, parameters, cv=5, n_jobs=-1, verbose=1)
+    # x and y are commonly used names for data set and class labels
+    x = texts_list
+    y = labels
+
+    print("len(x) ", len(x), "len(y) ", len(y))
+
+    # we use the same seed for repeatability
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=10002)
+
+    # todo wypisać statysytke tekstów (po labelach) w danych testowych i treningowych
+
+    grid_search = GridSearchCV(pipeline, parameters, cv=5, n_jobs=-1, verbose=10)
 
     print("Performing grid search...")
     print("pipeline:", [name for name, _ in pipeline.steps])
     print("parameters:")
     pprint(parameters)
     t0 = time()
-    grid_search.fit(new_texts_list, labels)
+    grid_search.fit(x_train, y_train)
     print("done in %0.3fs" % (time() - t0))
     print()
 
@@ -94,13 +89,24 @@ def main():
     for param_name in sorted(parameters.keys()):
         print("\t%s: %r" % (param_name, best_parameters[param_name]))
 
-    # summarize
-    # print("Vocabulary len:\n", len(vectorizer.vocabulary_))
-    # print("Vocabulary:\n", vectorizer.vocabulary_)
-    # print("Features len:\n", len(vectorizer.get_feature_names()))
-    # print("Feature names:\n", vectorizer.get_feature_names())
+    y_pred = grid_search.predict(x_test)
+    # todo wypisać ile jest poprawnych oszacowań
 
-    # encode document
+    # wiem ze tutaj moglem to zapisac do jednego pliku jako tuple, moze zmienie xd
+    with open(paths.last_grid_search_path, "wb"):
+        pickle.dump(grid_search)
+
+    with open(paths.x_test, "wb"):
+        pickle.dump(x_test)
+
+    with open(paths.x_train, "wb"):
+        pickle.dump(x_train)
+
+    with open(paths.y_test, "wb"):
+        pickle.dump(y_test)
+
+    with open(paths.y_train, "wb"):
+        pickle.dump(y_train)
 
 
 if __name__ == '__main__':
