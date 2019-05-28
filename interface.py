57a3aaf777b31064ff25daf7da664a49d6aa3372
Maciej Kasprzyk <kontakt@kasprzykmaciej.pl>
2019-05-28 22:32:33 +0200

    first version of interface and some texts to use

---
+++ interface.py
@@ -0,0 +1,39 @@
+import pickle
+from pprint import pprint
+
+from sklearn.model_selection import GridSearchCV
+import sys
+
+import paths
+
+
+def main():
+    file_name = sys.argv[1]
+
+    try:
+        with open(file_name, 'r') as file:
+            text = file.read()
+    except FileNotFoundError:
+        print("Nie ma takiego pliku lub niepoprawny plik")
+        exit(-1)
+
+    try:
+        with open(paths.interface_gird_search_path, 'rb') as file:
+            grid_search, x_test, y_test = pickle.load(file)
+    except FileNotFoundError:
+        print("Brak modelu do użycia na ścieżce: ", paths.interface_gird_search_path)
+        exit(-1)
+
+    classes = grid_search.classes_
+    dec_func = grid_search.decision_function([text])[0]
+    list = [(a, b) for a, b in zip(classes, dec_func)]
+    pprint(list)
+    # print(classes)
+    # print(dec_func)
+
+    predicted = grid_search.predict([text])
+    print(predicted)
+
+
+if __name__ == '__main__':
+    main()
